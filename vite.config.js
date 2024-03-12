86f02c16930721dbba94925c51edd4db5e8d5b7d
LontchiFrank <fodjof6@gmail.com>
2024-03-12 19:07:50 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: {},
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
   plugins: [react()],
 });
