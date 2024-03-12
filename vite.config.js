e5864b466728cb2500fc15fb1f70a42624c79431
LontchiFrank <fodjof6@gmail.com>
2024-03-12 19:23:43 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), InjectCSS()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
