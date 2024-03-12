07431654ba7844c04485830ada392df7ae8d95ac
LontchiFrank <fodjof6@gmail.com>
2024-03-12 19:35:23 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import InjectCSS from '@itsy/vite-css-inject';
-import path from 'path';
+import 'core-js-pure/stable/object/assign.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), InjectCSS()],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@tests': path.resolve(__dirname, './tests'),
-    },
-  },
+  plugins: [react()],
 });
