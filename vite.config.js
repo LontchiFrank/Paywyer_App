9512c17a68794c90df910d98689c4607fd9a6302
LontchiFrank <fodjof6@gmail.com>
2024-03-12 19:18:50 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import InjectCSS from '@itsy/vite-css-inject';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@tests': path.resolve(__dirname, './tests'),
+    },
+  },
 });
